21acbccffa286dd41d800ba7ea40f7dcae853ba9
Tommaso Soncin <soncintommaso@gmail.com>
2025-03-15 11:44:29 +0100

    chore: dump

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,35 +1,60 @@
 <script setup>
-  import { useLocale, useTheme } from 'vuetify'
+import { useI18n } from 'vue-i18n';
+import { useLocale, useTheme } from 'vuetify';
+import Drawer from '~/components/drawer/drawer.vue';
 
+const { setLocale } = useI18n();
+const { current } = useLocale();
+const theme = useTheme();
 
-  const { current } = useLocale()
-  const theme = useTheme()
+function checkUserLogin() {
+  return true;
+}
 
-  function toggleTheme () {
-    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
-  }
-  function changeLocale (locale) {
-    current.value = locale
-  }
+const isUserLoggedIn = checkUserLogin();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+}
+
+function changeLocale(locale) {
+  current.value = locale;
+}
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <router-link class="navbar-brand" to="/dashboard">Hospital San Commito</router-link>
-      <div class="collapse navbar-collapse">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        </ul>
-        <router-link to="/profile">
-          <button class="btn btn-outline-primary">Profile</button>
-        </router-link>
-      </div>
+  <v-app-bar>
+    <Drawer />
+    <v-app-bar-title class="text-truncate">
+      {{ $t('hospitalName') }}
+    </v-app-bar-title>
+    <v-spacer />
+    <div class="d-flex align-center gap-2">
+      <v-btn v-if="isUserLoggedIn" variant="outlined" :to="'/profile'">
+        <v-icon start>mdi-account-box</v-icon>
+        {{ $t('profile') }}
+      </v-btn>
+      <v-btn v-else color="primary" variant="outlined" :to="'/login'">
+        {{ $t('login') }}
+      </v-btn>
+      <v-btn icon @click="toggleTheme">
+        <v-icon>mdi-theme-light-dark</v-icon>
+      </v-btn>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-icon>mdi-translate</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item @click="setLocale('en')">
+            <v-list-item-title>English</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="setLocale('it')">
+            <v-list-item-title>Italiano</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </div>
-    <v-btn @click="toggleTheme">toggle theme</v-btn>
-    <v-btn @click="changeLocale">change locale</v-btn>
-
-  </nav>
+  </v-app-bar>
 </template>
-
-<style scoped>
-</style>
