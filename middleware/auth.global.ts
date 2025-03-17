1bbdd0f1624237feb8c83f743117f23bd221e203
Tommaso Soncin <soncintommaso@gmail.com>
2025-03-17 14:10:36 +0100

    feat: add types to auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,9 +1,13 @@
-import { useAuthStore } from '@/stores/authStore';
+import { useAuthStore } from "@/stores/authStore";
+import type { RouteLocationNormalized } from "vue-router";
 
-export default defineNuxtRouteMiddleware((to, from) => {
+export default defineNuxtRouteMiddleware((to: RouteLocationNormalized) => {
   const authStore = useAuthStore();
-  
-  if (!authStore.isValidToken() && !["/", "/login", "/signup"].includes(to.path)) {
+
+  if (
+    !authStore.isValidToken() &&
+    !["/", "/login", "/signup"].includes(to.path)
+  ) {
     console.log("[auth.global middleware] redirect to /login");
     return navigateTo("/login");
   }
