8c7ebedabc91eeef499af4d13304da831b04f2a6
Tommaso Soncin <soncintommaso@gmail.com>
2025-04-08 20:59:22 +0200

    chore: simplified env file
    Now we just load the variables directly, I do not want to touch this
    file anymore to fix it.

--- utils/env.ts
+++ utils/env.ts
@@ -1,27 +1,18 @@
-const config = useRuntimeConfig();
 interface Environment {
   AUTH_URL: string;
   PATIENTS_URL: string;
 }
-const env: Environment = { AUTH_URL: "", PATIENTS_URL: "" };
+
 // Yes, technically we're showing the user the env values.
 // However:
 // 1) We are not storing any sensitive information, we just didn't want to hard-code
 // any URL.
 // 2) There is a problem when we build for production, so we ensure that
 // we have the variables that we need from the environment here.
-if (process.env.NODE_ENV === "production") {
-  env.AUTH_URL =
-    (process.env.AUTH_URL as string) ||
-    config.public.AUTH_URL ||
-    "http://auth.sancommitto.local";
-  env.PATIENTS_URL =
-    (process.env.PATIENTS_URL as string) ||
-    config.public.PATIENTS_URL ||
-    "http://patients.sancommitto.local";
-} else {
-  env.AUTH_URL = (process.env.AUTH_URL as string) || config.public.AUTH_URL;
-  env.PATIENTS_URL =
-    (process.env.PATIENTS_URL as string) || config.public.PATIENTS_URL;
-}
+const env: Environment = {
+  AUTH_URL: (process.env.AUTH_URL as string) || "http://auth.sancommitto.local",
+  PATIENTS_URL:
+    (process.env.PATIENTS_URL as string) || "http://patients.sancommitto.local",
+};
+
 export default env;
