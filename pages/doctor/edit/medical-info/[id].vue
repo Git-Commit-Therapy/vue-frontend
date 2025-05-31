0be495de359ca68ca16d9d4f945e1717c969ecef
Tommaso Soncin <soncintommaso@gmail.com>
2025-05-31 11:34:48 +0200

    wip: updated pages to be single "do it all"

--- pages/doctor/edit/medical-info/[id].vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import type { Appointment } from "~/composable/protobuf/frontend/appointment";
-
-const raw = localStorage.getItem("currentAppointment");
-const appointment = raw ? (JSON.parse(raw) as Appointment) : null;
-</script>
-<template>
-  <div></div>
-</template>
--- utils/user-roles.ts
+++ utils/user-roles.ts
@@ -4,8 +4,8 @@ export function getUserRoles(jwtToken: string): string[] {
   if (!jwtToken) return [];
   try {
     const decodedToken = jwtDecode(jwtToken) as AuthToken;
-    return Array.isArray(decodedToken.groups ) ? decodedToken.groups  : [""];
-  } catch (error) {
+    return Array.isArray(decodedToken.groups) ? decodedToken.groups : [""];
+  } catch {
     return [""];
   }
 }
